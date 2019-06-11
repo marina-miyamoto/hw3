f4366cde65f37c48f3424abe079b36b2d39507c8
Marina Miyamoto <m.miyamoto0320@gmail.com>
2019-06-12 02:12:30 +0900

    Can Calculate Multiplication and Division

--- Calculator.py
+++ Calculator.py
@@ -1,85 +1,146 @@
-def readNumber(line, index):
-  number = 0
-  while index < len(line) and line[index].isdigit():
-    number = number * 10 + int(line[index])
-    index += 1
-  if index < len(line) and line[index] == '.':
-    index += 1
-    keta = 0.1
+def readNumber(line, index): #Separating Numbers
+    number = 0
     while index < len(line) and line[index].isdigit():
-      number += int(line[index]) * keta
-      keta /= 10
-      index += 1
-  token = {'type': 'NUMBER', 'number': number}
-  return token, index
+        number = number * 10 + int(line[index])
+        index += 1
+    if index < len(line) and line[index] == '.':
+        index += 1
+        keta = 0.1
+        while index < len(line) and line[index].isdigit():
+            number += int(line[index]) * keta
+            keta /= 10
+            index += 1
+    token = {'type': 'NUMBER', 'number': number}
+    return token, index
 
 
-def readPlus(line, index):
-  token = {'type': 'PLUS'}
-  return token, index + 1
+def readPlus(line, index): #Read Plus Sign
+    token = {'type': 'PLUS'}
+    return token, index + 1
 
 
-def readMinus(line, index):
-  token = {'type': 'MINUS'}
-  return token, index + 1
+def readMinus(line, index): #Read Minus Sign
+    token = {'type': 'MINUS'}
+    return token, index + 1
+
+def readMultiply(line, index): #Read Multiply Sign
+    token = {'type': 'MULTIPLY'}
+    return token, index + 1
+
+def readDivide(line, index): #Read Divide Sign
+    token = {'type': 'DIVIDE'}
+    return token, index + 1
 
 
-def tokenize(line):
-  tokens = []
-  index = 0
-  while index < len(line):
-    if line[index].isdigit():
-      (token, index) = readNumber(line, index)
-    elif line[index] == '+':
-      (token, index) = readPlus(line, index)
-    elif line[index] == '-':
-      (token, index) = readMinus(line, index)
-    else:
-      print('Invalid character found: ' + line[index])
-      exit(1)
-    tokens.append(token)
-  return tokens
+def tokenize(line): #Change the numbers/signs to tokens
+    tokens = []
+    index = 0
+    while index < len(line):
+        if line[index].isdigit():
+            (token, index) = readNumber(line, index)
+        elif line[index] == '+':
+            (token, index) = readPlus(line, index)
+        elif line[index] == '-':
+            (token, index) = readMinus(line, index)
+        elif line[index] == '*':
+            (token, index) = readMultiply(line, index)
+        elif line[index] == '/':
+            (token, index) = readDivide(line, index)
+        else:
+            print('Invalid character found: ' + line[index])
+            exit(1)
+        tokens.append(token)
+    return tokens
 
+def evaluate_multiplication_division(tokens):
+    tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
+    index = 1
+    while index < len(tokens):
+        if tokens[index]['type'] == 'NUMBER':
+            if tokens[index - 1]['type'] == 'MULTIPLY':
+                new_number = tokens[index - 2]['number'] * tokens[index]['number']
+                del tokens[index]
+                del tokens[index - 1]
+                del tokens[index - 2]
+                token = {'type': 'NUMBER', 'number': new_number}
+                tokens.insert(index - 2, token)
+                index = index - 2
+               
+                
+            elif tokens[index - 1]['type'] == 'DIVIDE':
+                new_number = tokens[index - 2]['number'] / tokens[index]['number']
+                del tokens[index]
+                del tokens[index - 1]
+                del tokens[index - 2]
+                token = {'type': 'NUMBER', 'number': new_number}
+                tokens.insert(index - 2, token)
+                index = tokens.index(token)
+                
+            elif tokens[index - 1]['type'] == 'PLUS' or tokens[index - 1]['type'] == 'MINUS':
+                tokens
 
-def evaluate(tokens):
-  answer = 0
-  tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
-  index = 1
-  while index < len(tokens):
-    if tokens[index]['type'] == 'NUMBER':
-      if tokens[index - 1]['type'] == 'PLUS':
-        answer += tokens[index]['number']
-      elif tokens[index - 1]['type'] == 'MINUS':
-        answer -= tokens[index]['number']
-      else:
-        print('Invalid syntax')
-        exit(1)
-    index += 1
-  return answer
+            else:
+                print('Invalid syntax')
+                exit(1)
+        index += 1
+    return tokens
+
+def evaluate_addition_subtraction(tokens):
+    answer = 0
+    index = 1
+    while index < len(tokens):
+        if tokens[index]['type'] == 'NUMBER':
+            if tokens[index - 1]['type'] == 'PLUS':
+                answer += tokens[index]['number']
+            elif tokens[index - 1]['type'] == 'MINUS':
+                answer -= tokens[index]['number']
+            else:
+                print('Invalid syntax')
+                exit(1)
+        index += 1
+    return answer
 
 
 def test(line):
-  tokens = tokenize(line)
-  actualAnswer = evaluate(tokens)
-  expectedAnswer = eval(line)
-  if abs(actualAnswer - expectedAnswer) < 1e-8:
-    print("PASS! (%s = %f)" % (line, expectedAnswer))
-  else:
-    print("FAIL! (%s should be %f but was %f)" % (line, expectedAnswer, actualAnswer))
+    tokens = tokenize(line)
+    tokens = evaluate_multiplication_division(tokens)
+    actualAnswer = evaluate_addition_subtraction(tokens)
+    expectedAnswer = eval(line)
+    if abs(actualAnswer - expectedAnswer) < 1e-8:
+        print("PASS! (%s = %f)" % (line, expectedAnswer))
+    else:
+        print("FAIL! (%s should be %f but was %f)" % (line, expectedAnswer, actualAnswer))
 
 
 # Add more tests to this function :)
 def runTest():
-  print("==== Test started! ====")
-  test("1+2")
-  test("1.0+2.1-3")
-  print("==== Test finished! ====\n")
+    print("==== Test started! ====")
+    test("1+2")
+    test("1.0+2.1-3")
+    test("1.5+8*2")
+    test("0.5+8/2")
+    test("1-2")
+    test("1-5+3")
+    test("1-9.8*2")
+    test("6-8/2")
+    test("7*3")
+    test("5*5+3")
+    test("1*9.8-2")
+    test("6*8/2")
+    test("1/2")
+    test("1/5+3")
+    test("1/9.8-2")
+    test("1/8*2")
+    test("1/8/2")
+    test("1+2-3*4")
+    print("==== Test finished! ====\n")
 
 runTest()
 
 while True:
-  print('> ', end="")
-  line = input()
-  tokens = tokenize(line)
-  answer = evaluate(tokens)
-  print("answer = %f\n" % answer)
+    print('> ', end="")
+    line = input()
+    tokens = tokenize(line)
+    tokens = evaluate_multiplication_division(tokens)
+    answer = evaluate_addition_subtraction(tokens)
+    print("answer = %f\n" % answer)   
